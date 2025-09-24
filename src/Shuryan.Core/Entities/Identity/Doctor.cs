9f4ac1967be9e85da26490ec2185b50928dd89eb
Abdulrhman Alaa <boda.alaa.eldfy@gmail.com>
2025-09-24 11:11:04 +0300

    Implement Some of Core Entity Models

---
+++ src/Shuryan.Core/Entities/Identity/Doctor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shuryan.Core.Entities.Common;
+using Shuryan.Core.Entities.Medical;
+using Shuryan.Core.Entities.System;
+using Shuryan.Core.Enums;
+
+namespace Shuryan.Core.Entities.Identity
+{
+	public class Doctor : ProfileUser
+	{
+		public MedicalSpecialty MedicalSpecialty { get; set; }
+		public int YearsOfExperience { get; set; }
+
+		// Optional for Credibility
+		public string? Biography { get; set; }
+
+		// Verification
+		public VerificationStatus VerificationStatus { get; set; } = VerificationStatus.Unverified;
+		public DateTime? VerifiedAt { get; set; }
+
+		[ForeignKey("Verifier")]
+		public Guid? VerifierId { get; set; }
+
+		// Navigation Properties
+		public virtual DoctorVerifier? Verifier { get; set; }
+		public virtual Clinic? Clinic { get; set; }
+		public virtual ICollection<DoctorService> Services { get; set; } = new HashSet<DoctorService>();
+		public virtual ICollection<DoctorOverride> Overrides { get; set; } = new HashSet<DoctorOverride>();
+		public virtual ICollection<DoctorAvailability> Availabilities { get; set; } = new HashSet<DoctorAvailability>();
+		public virtual ICollection<VerificationDocument> VerificationDocuments { get; set; } = new HashSet<VerificationDocument>();
+	}
+}
---
+++ src/Shuryan.Core/Entities/Identity/ProfileUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shuryan.Core.Enums;
+
+namespace Shuryan.Core.Entities.Identity
+{
+	public abstract class ProfileUser : User
+	{
+		public DateTime? BirthDate { get; set; } // Optional for Credibility
+		public Gender? Gender { get; set; } // Optional for Credibility
+		public string? ProfileImageUrl { get; set; } // URL to the user's profile image -> Optional for Credibility
+	}
+}
