690e44632455dd54e6932ce9d54fdd7a29405919
Abdulrhman Alaa <boda.alaa.eldfy@gmail.com>
2025-09-27 18:54:10 +0300

    Setup DbContext

---
+++ src/Shuryan.Infrastructure/Data/Configurations/ProfileUserEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Shuryan.Core.Entities.Identity;
+
+namespace Shuryan.Infrastructure.Data.Configurations
+{
+	public class ProfileUserEntityConfiguration : IEntityTypeConfiguration<ProfileUser>
+	{
+		public void Configure(EntityTypeBuilder<ProfileUser> builder)
+		{
+			builder.Property(pu => pu.Gender)
+				   .HasConversion<int>();
+		}
+	}
+}
---
+++ src/Shuryan.Infrastructure/Data/Configurations/VerificationDocumentEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Shuryan.Core.Entities.Common;
+
+namespace Shuryan.Infrastructure.Data.Configurations
+{
+	public class VerificationDocumentEntityConfiguration : IEntityTypeConfiguration<VerificationDocument>
+	{
+		public void Configure(EntityTypeBuilder<VerificationDocument> builder)
+		{
+			builder.HasKey(vd => vd.Id);
+
+			builder.Property(vd => vd.Type)
+				   .HasConversion<int>()
+				   .IsRequired();
+
+			// Relationships
+			builder.HasOne(vd => vd.Doctor)
+				   .WithMany(d => d.VerificationDocuments)
+				   .HasForeignKey(vd => vd.DoctorId)
+				   .OnDelete(DeleteBehavior.Cascade);
+		}
+	}
+}
