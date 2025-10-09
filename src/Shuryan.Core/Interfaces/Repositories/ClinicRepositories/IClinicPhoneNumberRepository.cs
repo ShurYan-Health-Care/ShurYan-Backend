fe2e0d3c2498b5d2c6e7564c3d7f90395bd233fd
Akl-n1 <ma7mud3kl@gmail.com>
2025-10-09 04:31:47 +0300

    add comment

--- src/Shuryan.Core/Interfaces/Repositories/ClinicRepositories/IClinicPhoneNumberRepository.cs
+++ src/Shuryan.Core/Interfaces/Repositories/ClinicRepositories/IClinicPhoneNumberRepository.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Shuryan.Core.Interfaces.Repositories.ClinicRepositories
 {
+    // Repository interface for managing ClinicPhoneNumber entities
     public interface IClinicPhoneNumberRepository : IGenericRepository<ClinicPhoneNumber>
     {
         Task<IEnumerable<ClinicPhoneNumber>> GetClinicPhoneNumbersAsync(Guid clinicId);
