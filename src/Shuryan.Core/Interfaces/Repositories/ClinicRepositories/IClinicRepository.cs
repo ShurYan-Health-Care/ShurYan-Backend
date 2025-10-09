fc67da8446d955e9c7601a2ee2b7f802714365f7
Akl-n1 <ma7mud3kl@gmail.com>
2025-10-09 04:19:02 +0300

    Update ClinicRepository

---
+++ src/Shuryan.Core/Interfaces/Repositories/ClinicRepositories/IClinicRepository.cs
@@ -0,0 +1,20 @@
+﻿using Shuryan.Core.Entities.External.Clinic;
+using Shuryan.Core.Enums;
+using Shuryan.Core.Enums.Clinic;
+using Shuryan.Core.Enums.Doctor;
+using Shuryan.Core.Enums.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shuryan.Core.Interfaces.Repositories.ClinicRepositories
+{
+    public interface IClinicRepository : IGenericRepository<Clinic>
+    {
+
+        Task<Clinic?> GetClinicByDoctorIdAsync(Guid doctorId);
+
+        Task<IEnumerable<Clinic>> GetClinicsNearLocationAsync(double latitude, double longitude, double radiusInKm);
+
+    }
+}
